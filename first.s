859440a17f33bf7d682b4293dff15986eab6ce9a
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2023-10-03 17:07:29 -0400

    Update first.s

--- first.s
+++ first.s
@@ -1,8 +1,12 @@
-/* -- first.s */
-/* This is a comment */
-.global main	/* entry point must be global */
-.func main	/* 'main' is a funtion */
+/*
+Curtis Edwards
+TEJ4M-Unit-2-04
+This program returns the number 1
+*/
 
-main:		/* This is main */
-    mov r0, #1  /* Put a 1 into register r0 */
-    bx lr       /* Return from main */
+.global main   // entry point must be global
+.func main	   //'main' is a funtion
+
+main:		   //This is main
+    mov r0, #1 // Put a 1 into register r0
+    bx lr      // Return from main
