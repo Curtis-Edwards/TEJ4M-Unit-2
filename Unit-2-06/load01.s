9cd0e4988c27a7bf6fecb34b23f82a07aed1fc7e
curtis <curtis.edwards@mths.ca>
2023-10-17 13:40:58 -0400

    Added load01.s file

--- Unit-2-06/load01.s
+++ Unit-2-06/load01.s
@@ -18,3 +18,19 @@ myvar2:
 	.word 4
 
 /* -- Code section */
+.text
+
+/* Ensure variable is 4-byte aligned */
+.balign 4
+.global main
+main:
+	ldr r1, addr_of_myvar1 // r1 <- &myvar1
+	ldr r1, [r1]           // r1 <- *r1
+	ldr r2, addr_of_myvar2 // r2 <- &myvar2
+	ldr r2, [r2]           // r2 <- *r2
+	add r0, r1, r2         // r0 <- r1 + r2
+	bx lr
+
+/*Labels needed to access data */
+addr_of_myvar1: .word myvar1
+addr_of_myvar2: .word myvar2
