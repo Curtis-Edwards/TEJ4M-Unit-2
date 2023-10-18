9f882c121633d20155da61ca5dd3d7661862eb76
curtis <curtis.edwards@mths.ca>
2023-10-18 17:21:20 -0400

    Added code

--- Unit-2-06/store01.s
+++ Unit-2-06/store01.s
@@ -3,7 +3,6 @@
 /* -- Data section */
 .data
 
-S: .asciz "string!"
 /* Ensure variable is 4-byte aligned */
 .align 4
 /* Define storage for myvar1 */
@@ -33,9 +32,9 @@ mov r3, #4 /* r3 <- 4 */
 str r3, [r2] /* *r2 <- r3 */
 
 /* Same instructions as above */
-ldr r1, addr_of_myvar1 /* r1 <- &myvar1 */
+// ldr r1, addr_of_myvar1 /* r1 <- &myvar1 */
 ldr r1, [r1] /* r1 <- *r1 */
-ldr r2, addr_of_myvar2 /* r2 <- &myvar2 */
+// ldr r2, addr_of_myvar2 /* r2 <- &myvar2 */
 ldr r2, [r2] /* r2 <- *r2 */
 add r0, r1, r2
 bx lr
