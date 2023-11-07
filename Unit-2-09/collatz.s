887a3f5db1a7e9f243dfbb318c582f3c7f64e927
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2023-11-07 17:47:51 -0500

    Update collatz.s

--- Unit-2-09/collatz.s
+++ Unit-2-09/collatz.s
@@ -1,15 +1,27 @@
-/* -- compare01.s */
+/* -- collatz.s */
 .text
 .global main
 main:
-	mov r1, #2       // r1 <- 2
-	mov r2, #-2      // r2 <- -2
-	cmn r1, r2
-	beq case_equal   // branch to case_equal only if Z = 1
-case_different:
-	mov r0, #2       // r0 <- 2
-	b end            // branch to end
-case_equal:
-	mov r0, #1       // r0 <- 1
+	mov r1, #123 // r1 <- 123 a trial number
+	mov r2, #0 // r2 <- 0 the # of steps
+loop:
+	cmp r1, #1 // compare r1 and 1
+	beq end // branch to end if r1 == 1
+
+	and r3, r1, #1 // r3 <- r1 & 1 [mask]
+	cmp r3, #0 // compare r3 and 0
+	bne odd // branch to odd if r3 != 0
+even:
+	mov r1, r1, ASR #1 // r1 <- (r1 >> 1) [divided by 2]
+	b end_loop
+odd:
+	add r1, r1, r1, LSL #1 // r1 <- r1 + (r1 << 1) [3n]
+	add r1, r1, #1 // r1 <- r1 + 1 [3n+1]
+
+end_loop:
+	add r2, r2, #1 // r2 <- r2 + 1
+	b loop // branch to loop
+
 end:
+	mov r0, r2 // number of steps
 	bx lr
