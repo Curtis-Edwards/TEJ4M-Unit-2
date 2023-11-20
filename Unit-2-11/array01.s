24949387b3a26e088ae2de83d8332dc811283a58
curtis <curtis.edwards@mths.ca>
2023-11-20 17:51:54 -0500

    Finished Unit-2-11

--- Unit-2-11/array01.s
+++ Unit-2-11/array01.s
@@ -5,15 +5,15 @@ b: .skip 8
 .text
 .global main
 main:
-	ldr r1, =a @ r1 <- &a
-	mov r2, #0 @ r2 <- 0
-
+	ldr r1, =a // r1 <- &a
+	mov r2, #0 // r2 <- 0
 Loop:
-	cmp r2, #100 @ Have we reached 100 yet?
-	beq end @ If so, leave the loop
-	add r3, r1, r2, LSL #2 @ r3 <- r1 + (r2*4)
-	str r2, [r3] @ *r3 <- r2
-	add r2, r2, #1 @ r2 <- r2 + 1
-	b Loop @ Goto beginning of the Loop
+	cmp r2, #100 // Have we reached 100 yet?
+	beq end // If so, leave the loop
+	ldr r2, [r1, #+12]! // r1 <- r1 + 12 then r2 <- *r1
+	add r2, r2, r2  // r2 <- r2 + r2
+	str r2, [r1] // *r1 <- r2 : r1 now = address of a[3]
+	add r2, r2, #1 // r2 <- r2 + 1
+	b Loop // Goto beginning of the Loop
 end:
 	bx lr
