4f853bcf1d09aca90be90b11ad8750d36badf908
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2023-11-29 12:47:45 -0500

    Update numericalLabels.s

--- Unit-2-12/numericalLabels.s
+++ Unit-2-12/numericalLabels.s
@@ -1,51 +1,25 @@
-/* -- printf01.s */
-.data
-
-/* First message */
-.balign 4
-message1: .asciz "Hey, type a number: "
-
-/* Second message */
-.balign 4
-message2: .asciz "I read the number %d\n"
-
-/* Format pattern for scanf */
-.balign 4
-scan_pattern : .asciz "%d"
-
-/* Where scanf will store the number read */
-.balign 4
-number_read: .word 0
-
-.balign 4
-return: .word 0
-
+/* -- numericalLabels.s */
+.global main /* entry point must be global */
+.syntax unified /* modern syntax (UAL=Unified Assembler Language) */
 .text
+main: /* This is main */
+  push {r4, lr}
+  ldr r0, =message1
+  bl puts
+  b 1f /* Goto the first label 1 forward */
+  b 2f /* Goto the first label 2 forward */
+1:
+  ldr r0, =message2
+  bl puts
+1:
+  mov r0, 43 /* The # is optional in UAL */
+  pop {r4, pc} /* Return from main */
+2:
+  ldr r0, =errmessage
+  bl puts
+  b 1b /* Goto the first label 1 backward */
 
-.global main
-main:
-ldr r1, =return // r1 <- &return
-str lr, [r1] // *r1 <- lr ; save return address
-
-ldr r0, =message1 // r0 <- &message1
-bl printf // call to printf
-
-ldr r0, =scan_pattern // r0 <- &scan_pattern
-ldr r1, =number_read // r1 <- &number_read
-bl scanf // call to scanf
-
-ldr r0, =message2 // r0 <- &message2
-ldr r1, =number_read // r1 <- &number_read
-ldr r1, [r1] // r1 <- *r1
-bl printf // call to printf
-
-ldr r0, =number_read // r0 <- &number_read
-ldr r0, [r0] // r0 <- *r0
-
-ldr lr, =return // lr <- &return
-ldr lr, [lr] // lr <- *lr
-bx lr // return from main using lr
-
-/* External */
-.global printf
-.global scanf
+.data
+message1: .asciz "Numerical Label Test\n"
+message2: .asciz "Success\n"
+errmessage: .asciz "Failure!\n".global puts
