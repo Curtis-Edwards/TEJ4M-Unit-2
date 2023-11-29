1c33a8a6e8bab39fe635ceb394d4aa0df1953978
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2023-11-29 12:45:19 -0500

    Update hello01.s

--- Unit-2-12/hello01.s
+++ Unit-2-12/hello01.s
@@ -11,18 +11,18 @@ return: .word 0
 
 .global main
 main:
-ldr r1, =return // r1 <- &return
-str lr, [r1] // *r1 <- lr
-
-ldr r0, =greeting // r0 <- &greeting
-// First parameter of puts
-
-bl puts // Call to puts
-// lr <- address of next instruction
-
-ldr r1, =return // r1 <- &return
-ldr lr, [r1] // lr <- *r1
-bx lr // return from main
+  ldr r1, =return // r1 <- &return
+  str lr, [r1] // *r1 <- lr
+  
+  ldr r0, =greeting // r0 <- &greeting
+  // First parameter of puts
+  
+  bl puts // Call to puts
+  // lr <- address of next instruction
+  
+  ldr r1, =return // r1 <- &return
+  ldr lr, [r1] // lr <- *r1
+  bx lr // return from main
 
 /* External */
 .global puts // The C function puts
